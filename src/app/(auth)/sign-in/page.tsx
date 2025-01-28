2e949c34d1a0517aa7cafba559ff00453cb11ba5
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2025-01-28 15:00:36 -0600

    auth screen

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+const SignInPage = () => {
+  return <div>Sign in Page</div>;
+};
+
+export default SignInPage;
+
+// 1. important to do default export because export const is not a react component
+// 2. important to do export default because it is a react component
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+const SignUpPage = () => {
+  return <div>Sign Up Page</div>;
+};
+
+export default SignUpPage;
+
+// 1. important to do default export because export const is not a react component
+// 2. important to do export default because it is a react component

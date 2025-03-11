d530780745d23f13a8f02b06ea03130416b71148
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2025-03-11 18:14:26 -0500

    finishing authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,21 @@
 import { useRouter } from "next/navigation";
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useEffect } from "react";
-
+import { useLogout } from "@/features/auth/api/use-logout";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   const { data, isLoading } = useCurrent();
   const router = useRouter();
+  const { mutate: logout } = useLogout();
   useEffect(() => {
     if (!data && !isLoading) {
       router.push("/sign-in");
     }
   }, [data]);
-  return <div className="flex gap-4">only visible to authenticated users</div>;
+  return (
+    <div className="flex gap-4">
+      only visible to authenticated users
+      <Button onClick={() => logout()}>Logout</Button>
+    </div>
+  );
 }

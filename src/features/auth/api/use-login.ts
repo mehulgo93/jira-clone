d546e9fb160c03fd3f8c38aa4850583f0c3f2df8
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2025-03-11 19:29:48 -0500

    authentication complete

--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -1,7 +1,8 @@
-import { Mutation, useMutation } from "@tanstack/react-query";
+import { Mutation, useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType } from "hono";
 import { InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 
 type ResponseType = InferResponseType<typeof client.api.auth.login["$post"]>;
@@ -9,10 +10,16 @@ type ResponseType = InferResponseType<typeof client.api.auth.login["$post"]>;
 type RequestType = InferRequestType<typeof client.api.auth.login["$post"]>;
 
 export const useLogin = () => {
+    const queryClient = useQueryClient();
+    const router = useRouter();
     const mutation = useMutation<ResponseType, Error, RequestType>({
         mutationFn: async ({json}) => {
             const response = await client.api.auth.login["$post"]({json});
             return await response.json();
+        },
+        onSuccess: () => {
+            router.refresh();
+            queryClient.invalidateQueries({queryKey: ["current"]});
         }
     })
 
--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,13 +1,13 @@
 import { Mutation, useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
-
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>;
 
 
 export const useLogout = () => {
-
+    const router = useRouter();
     const queryClient = useQueryClient();
     const mutation = useMutation<ResponseType, Error>({
         mutationFn: async () => {
@@ -15,7 +15,7 @@ export const useLogout = () => {
             return await response.json();
         },
         onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: ["current"]});
+            router.refresh();
         }
     })
 

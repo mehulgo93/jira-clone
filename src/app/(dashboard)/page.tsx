78448e0c7781684e83e8e94a2cfdd185c0879fe7
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2025-03-17 01:35:02 -0500

    creating workspace form

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,16 @@
 import { redirect } from "next/navigation";
 import { getCurrent } from "@/features/auth/actions";
+import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 
 export default async function Home() {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
-  return <div>This is the home page</div>;
+  return (
+    <div className="bg-neutral-500 p-4 h-full">
+      <CreateWorkspaceForm />
+    </div>
+  );
 }
 
 // await or asynchornous functions can only be used in the server components
---
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormProvider, useForm } from "react-hook-form";
+import { createWorkspaceSchema } from "../schemas";
+import { z } from "zod";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { DottedSeparator } from "@/components/dotted-separator";
+interface CreateWorkspaceFormProps {
+  onCancel?: () => void;
+}
+
+export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
+  const methods = useForm();
+  const form = useForm<z.infer<typeof createWorkspaceSchema>>({
+    resolver: zodResolver(createWorkspaceSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="flex p-7">
+        <CardTitle className="text-xl font-bold">
+          Create a new workspace
+        </CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <FormProvider {...methods}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Workspace Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter workspace name" />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </FormProvider>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};

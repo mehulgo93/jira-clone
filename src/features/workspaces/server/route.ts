73fc5a30f75d9f58393f47a4064cba245b53c91d
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2025-04-08 00:49:11 -0500

    handling image upload

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { createWorkspaceSchema } from "../schemas";
-import { ID } from "node-appwrite";
+import { ID, ImageGravity } from "node-appwrite";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 
 const app = new Hono()
 .post(
@@ -12,12 +12,24 @@ const app = new Hono()
     sessionMiddleware,
     async (c) => {
         const databases = c.get("databases");
+        const storage = c.get("storage");
         const user = c.get("user");
 
-        const {name} = c.req.valid("form");
+        const {name, image} = c.req.valid("form");
+        let uploadedImageUrl: string | undefined;
+
+        if (image instanceof File) {
+            const file = await storage.createFile(IMAGES_BUCKET_ID, ID.unique(), image);
+            const arrayBuffer = await storage.getFilePreview(
+                IMAGES_BUCKET_ID,
+                file.$id,
+            )
+            uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+        }
         const workspace = await databases.createDocument(DATABASE_ID, WORKSPACES_ID, ID.unique(), {
             name,
             userId: user.$id,
+            image: uploadedImageUrl,
         });
 
         return c.json({data: workspace});
